d44376b44d17ffb71b7959e506b741f943aee9de
Dmitri Samotessov <dimasamo96@gmail.com>
2018-10-11 16:44:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,6 +4,6 @@ function addElementToBeginningOfArray(array, element){
   return [element,...array]
 }
 function destructivelyAddElementToBeginningOfArray(array, element){
-  array.unshit(element)
+  array.unshift(element)
   return array
 }
